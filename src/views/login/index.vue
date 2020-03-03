8dc7d73b57b11ca1cbf7f9ed9f8d794eb5976cb5
unknown <you@example.com>
2020-03-04 00:52:53 +0800

    表单规则编写-自动校验规则

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,7 +8,7 @@
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
       <!-- 表单 绑定model属性 绑定rules属性（表单验证规则）-->
-      <el-form :model="loginForm" :loginRules="rules" style="margin-top:20px">
+      <el-form :model="loginForm" :rules="loginRules" style="margin-top:20px">
         <!-- 表单容器 设置prop属性表示要校验的字段名-->
         <el-form-item prop="mobile">
           <!-- 表单域 v-model双向绑定-->
@@ -47,7 +47,27 @@ export default {
       },
       // 定义表单验证规则
       loginRules: {
-
+        // required表示该字段必填
+        mobile: [{ required: true, message: '您的手机号不能为空' }, {
+          pattern: /^1[3-9]\d{9}$/, // 手机号码正则表达式
+          message: '您的手机号格式不正确'
+        }],
+        code: [{ required: true, message: '您的验证码不能为空' }, {
+          pattern: /^\d{6}$/, // 验证码要求6个数字
+          message: '您的验证码不正确'
+        }],
+        // 自定义校验
+        checked: [{
+          validator: function (rule, value, callback) {
+          // rule是当前校验规则
+          // value是当前检验字段的值
+          // callback是一个回调函数 不论成功或失败都要执行
+          // 成功执行callback 失败执行 callback(new Error('错误信息'))
+          // 如果value为true就表示校验成功 false为失败
+          // new Error(错误信息) 就是我们提示的错误信息
+            value ? callback() : callback(new Error('您必须同意我们的协议'))
+          }
+        }]
       }
     }
   }
